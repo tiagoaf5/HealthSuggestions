beed30dedccaa4f4fe48dded785f941d02d25f3c
Tiago Fernandes <ei11054@fe.up.pt>
2015-02-10 20:50:41 +0000

    Done some progress  - just working on google  - picks search text  - displays accross next pages even if new tab is opened  - design needs reviews of course

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,13 +1,163 @@
 // if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
- var settings = new Store("settings", {
-     "sample_setting": "This is how you use Store.js to remember values"
+/*var settings = new Store("settings", {
+ "sample_setting": "This is how you use Store.js to remember values"
  });
+ */
+
+var SEARCH = "search";
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+        "from a content script:" + sender.tab.url :
+            "from the extension");
+        console.log("request: " + JSON.stringify(request));
+
+        if (request.greeting == "hello") {
+            sendResponse({farewell: "goodbye"});
+        }
+
+        if (!request.action) {
+            console.warn('invalid command: ', request);
+            return;
+        }
+
+        switch (request.action) {
+            case 'getTabId':
+                sendResponse({tabId: sender.tab.id});
+                break;
+            case 'createEntry':
+                /* if(chrome.storage.local.get(SEARCH + sender.tab.id) == chrome.runtime.lastError)
+                 sendResponse();
+
+                 chrome.storage.local.set({'value': theValue}*/
+                console.info("Creating entry...");
+                var obj = {};
+                obj[SEARCH + sender.tab.id] = null;
+                chrome.storage.local.set(obj);
+                break;
+            case 'ready':
+                var obj = {};
+                obj[SEARCH + sender.tab.id] = null;
+
+                chrome.storage.local.get(obj, function(result) {
+                    //Checks if this tab was used to do a search
+                    console.log("ready result: " + JSON.stringify(result));
+                    if (result[SEARCH + sender.tab.id])
+                        notifyTabOfState(sender.tab.id);
+
+                });
+                break;
+            case 'updateData':
+                console.info("Creating entry...");
+                var obj = {};
+                obj[SEARCH + sender.tab.id] = request.data;
+                chrome.storage.local.set(obj);
+                break;
+            case 'loadData':
+                var obj = {};
+                obj[SEARCH + sender.tab.id] = null;
+
+                chrome.storage.local.get(obj, function(result) {
+                    //Checks if this tab was used to do a search
+                    console.log("loadData result: " + JSON.stringify(result));
+                    if (result[SEARCH+sender.tab.id])
+                        chrome.tabs.sendMessage(sender.tab.id, {action: "setData", query: result[SEARCH+sender.tab.id]});
+                    else
+                        console.log("loadData error");
+
+                });
+                break;
+
+            default:
+                console.warn('Unknown request: ', request);
+
+        }
+    });
+
+/**
+ * used instead of chrome.tabs.onCreated.addListener(function(details)
+ * because this one isn't triggered when a user opens a new empty tab
+ * just when a tab is opened from a link in another tab
+ */
+chrome.webNavigation.onCreatedNavigationTarget.addListener(function (details) {
+    console.log("------------------");
+    console.log("sourceTabId: " + details.sourceTabId);
+    console.log("------------------");
 
 
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+    console.log("Created new tab with ID: " + details.tabId);
+
+
+    console.log("parent: " + details.sourceTabId);
+    chrome.tabs.query({active: true}, function (res) {
+        console.log("activeTab: " + res[0].id);
+
+    });
+    var obj = {};
+    obj[SEARCH + details.sourceTabId] = null;
+
+    chrome.storage.local.get(obj, function(res) {
+        console.log("onCreated: " + JSON.stringify(res));
+        if(res[SEARCH + details.sourceTabId] != null) {
+            var newO = {};
+            newO[SEARCH + details.tabId] = res[SEARCH + details.sourceTabId];
+            chrome.storage.local.set(newO);
+
+            notifyTabOfState(details.tabId);
+        }
+    });
+
+});
+
+chrome.tabs.onRemoved.addListener(
+    function(tabId) {
+        chrome.storage.local.remove(SEARCH + tabId);
+
+
+    });
+
+function notifyTabOfState(id) {
+    //console.log('notifyTabOfState message to ',id, active,on);
+    console.log("Sending message...");
+    chrome.tabs.sendMessage(id, {
+        'action': 'tabstate'
+    });
+}
+/*
+
+ //example of using a message handler from the inject scripts
+ chrome.runtime.onMessage.addListener(
+ function(request, sender, sendResponse) {
+
+ console.log("benfica");
+ sendResponse({farewell: "goodbye"});
+ if(!request.action)
+ {
+ console.warn('invalid command: ',request);
+ return;
+ }
+
+ console.log('new message: '+request.action, request);
+
+ switch (request.action) {
+ case 'getTabId':
+ sendResponse(sender.tab.id);
+ break;
+ default:
+ console.warn('Unknown request: ',request);
+ }
+
+ //chrome.pageAction.show(sender.tab.id);
+ //sendResponse();
+ });*/
+/*
+ chrome.runtime.onMessage.addListener(
+ function(request, sender, sendResponse) {
+ console.log(sender.tab ?
+ "from a content script:" + sender.tab.url :
+ "from the extension");
+ if (request.greeting == "hello")
+ sendResponse({farewell: "goodbye"});
+ });*/
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,69 +1,64 @@
-chrome.extension.sendMessage({}, function(response) {
-    var readyStateCheckInterval = setInterval(function() {
-        if (document.readyState === "complete") {
-            clearInterval(readyStateCheckInterval);
-
-            // ----------------------------------------------------------
-            // This part of the script triggers when page is done loading
-            console.log("Hello. This message was sent from scripts/inject.js");
-            // ----------------------------------------------------------
-
-
-            /*var iframe = document.createElement('div');
-             // Must be declared at web_accessible_resources in manifest.json
-             iframe.id = "myiframe";
-             iframe.src = chrome.runtime.getURL('src/inject/layout.html');
-             iframe.scrolling = "no";
-             iframe.style.cssText = "position: fixed !important; \
-             overflow: hidden;\
-             margin: 0px;\
-             width: 350px; \
-             height: 250px;\
-             z-index: 1010101;\
-             border: none ! important;\
-             bottom: 0px;\
-             right: 15px;";*/
-            var url = window.location.href;
-
-            if(/^https?:\/\/www\.google\.\w{1,3}(\/.*)?/.test(url)) {
-                loadWidget();
-                $("#gbqfq").on("change", function() {
-                    console.info($("#gbqfq").val());
-                    $("#query").text($("#gbqfq").val());
-
-                });
-            }
+$(document).ready(function() {
+    var url = window.location.href;
 
 
 
 
 
+    if(/^https?:\/\/www\.google\.\w{1,3}(\/.*)?/.test(url) && url.indexOf("newtab") == -1) {
+        loadWidget();
+        chrome.runtime.sendMessage({action: "createEntry"});
+        $("."+widgetClass).css("display", "inline-block");
+
+        setTimeout(function(){
+            $("#query").text($("#gbqfq").val());
+            chrome.runtime.sendMessage({action: "updateData", data: $("#gbqfq").val()});
+        }, 400);
+
+        //recordTabSearch()
+        $("#gbqfq").on("change", function() {
+            console.info($("#gbqfq").val());
+            $("#query").text($("#gbqfq").val());
+            chrome.runtime.sendMessage({action: "updateData", data: $("#gbqfq").val()});
+        });
+    }
+    else
+        chrome.runtime.sendMessage({action: "ready"});
+
+});
+
+var widgetClass = "widgetClass";
 
 
-
-
-
-            //Works
-            /*
-             var div = document.createElement('div');
-             var google = chrome.runtime.getURL('icons/google.ico');
-             var bing = chrome.runtime.getURL('icons/bing.png');
-             var yahoo = chrome.runtime.getURL('icons/yahoo.png');
-             div.style.cssText = 'z-index: 1010101;position: fixed;' +
-             'bottom: 0px;right: 50px;';
-             div.innerHTML = '<div id="frame"><div class="header"><div class="header-content"><ul class="search-engine"><li><a href="#"><img src="' +
-             google + '"></a></li><li><a href="#"><img src="' +
-             bing + '"></a></li><li><a href="#"><img src="' +
-             yahoo + '"></a></li></ul><span class="minimize"><a href="#">x</a></span><span class="minimize"><a href="#">_</a></span></div></div><div class="body"><div class="body-content"><span>Pesquisa original: </span><span id="query" class="blue">nabo</span></div></div></div>';
-             document.body.appendChild(div);
-             */
-
-            //var content = '<div style=""><iframe src="chrome-extension://__MSG_@@extension_id__/src/inject/layout.html"></iframe></div>';
-            //document.body.innerHTML += content;
+chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log('new message: '+ JSON.stringify(request));
+        if(!request.action)
+        {
+            console.log('invalid action',request);
+            return;
         }
 
-    }, 10);
-});
+
+
+        switch(request.action) {
+
+            case 'tabstate':
+
+                console.log("appending widget");
+                loadWidget();
+                $("."+widgetClass).css("display", "inline-block");
+
+                chrome.runtime.sendMessage({action: 'loadData'});
+                break;
+            case 'setData':
+                console.log("receiving result...: " + JSON.stringify(request.query));
+                $("#query").text(request.query);
+            default :
+                break;
+        }
+    });
+
 
 function loadWidget() {
     var widget = $("<div>");
@@ -75,9 +70,23 @@ function loadWidget() {
         "z-index": "1010101",
         "border": "none ! important",
         "bottom": "0px",
-        "right": "15px"});
+        "right": "15px",
+        "display": "none"});
+    widget.addClass(widgetClass);
 
     widget.load(chrome.runtime.getURL('src/inject/layout.html'));
-    $("body").append(widget);
+    $(document.body).append(widget);
     $("#query").text($("#gbqfq").val());
 }
+
+
+
+
+/*
+ function createStorageEntry() {
+ console.log("Creating storage entry...");
+ chrome.runtime.sendMessage({action: "createEntry"}, function (response) {
+ console.log("response: " + JSON.stringify(response));
+ });
+ }*/
+
