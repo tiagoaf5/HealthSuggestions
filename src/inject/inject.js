1a7485efbf5a7f5f236dbb40971057f456f5bcb7
Tiago Fernandes <ei11054@fe.up.pt>
2015-03-30 22:59:43 +0100

    added common-web library

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -18,6 +18,13 @@ var G_YAHOO_BASE_URL = "https://search.yahoo.com/search?p=";
 $(document).ready(function() {
     var url = window.location.href;
 
+    CommonWeb.Callback = function(collection, properties, callback) {
+        console.log("collectin: " + collection);
+        console.log("properties: " + JSON.stringify(properties));
+    };
+
+    CommonWeb.trackClicks();
+
     //if it's google
     if(/^https?:\/\/www\.google\.\w{1,3}(\/.*)?/.test(url) && url.indexOf("newtab") == -1) {
         console.log("It's google here!");
