d0b37666a80ebc1676494467e9706efccea61c02
Tiago Fernandes <ei11054@fe.up.pt>
2015-05-04 11:13:35 +0100

    changed settings to default use server ones

--- server/settings.py
+++ server/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'healthSuggestions',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': ',',
+        'USER': 'healthSuggestions',
+        'PASSWORD': 'password',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -106,8 +106,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+#
+STATIC_URL = "/opt/myenv/static/"
+
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
+
+try:
+    from local_settings import *
+except ImportError as e:
+    pass
