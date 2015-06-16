b7ce5b747d282717b101879882e994bee5f3919c
Tiago Fernandes <ei11054@fe.up.pt>
2015-06-16 15:08:49 +0100

    added models to log database

--- healthSuggestions/models.py
+++ healthSuggestions/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+#############################
+#Indexes for extension      #
+#############################
 
 class CHVConcept(models.Model):
     CUI = models.TextField(primary_key=True)
@@ -26,3 +29,128 @@ class CHVString(models.Model):
     pt_count = models.PositiveSmallIntegerField(blank=True)
     pt_stemmed_count = models.PositiveSmallIntegerField(blank=True)
     cui = models.ForeignKey('CHVConcept')
+
+#############################
+#      LOGGING DATABASE     #
+#                           #
+#     User and Sessions     #
+#############################
+
+class User(models.Model):
+    cookieID = models.UUIDField(primary_key=True)
+    registerDate = models.DateTimeField(auto_now_add=True)
+    browser = models.CharField(max_length=50, blank=True)
+    os = models.CharField(max_length=50, blank=True)
+
+
+class Session(models.Model):
+    cookieID = models.ForeignKey('User')
+    ip = models.GenericIPAddressField()
+    startTimestamp = models.DateTimeField(auto_now_add=True)
+
+#############################
+#         Suggestions       #
+#############################
+
+
+class SuggestionLanguage(models.Model):
+    language = models.CharField(max_length=20)
+    iso6391 = models.CharField(max_length=2)
+
+
+class SuggestionType(models.Model):
+    type = models.CharField(max_length=20)
+
+
+class Suggestion(models.Model):
+    suggestion = models.CharField(max_length=100)
+    suggestionLanguage = models.ForeignKey('SuggestionLanguage')
+    suggestionType = models.ForeignKey('SuggestionType')
+
+
+#############################
+#           Search          #
+#############################
+
+
+class SearchEngine(models.Model):
+    name = models.CharField(max_length=10)
+    url = models.URLField()
+
+
+class SERelatedSearch(models.Model):
+    suggestion = models.CharField(max_length=50)
+
+
+class Search(models.Model):
+    query = models.CharField(max_length=120)
+    queryInputTimestamp = models.DateTimeField(auto_now_add=True)
+    totalNoResults = models.PositiveIntegerField(blank=True)
+    answerTime = models.FloatField(blank=True)
+    session = models.ForeignKey('Session', related_name='searches')
+    suggestions = models.ManyToManyField('Suggestion', related_name='searches')
+    searchEngine = models.ForeignKey('SearchEngine')
+    seRelatedSearches = models.ManyToManyField('SERelatedSearch', related_name='searches')
+
+
+class SearchPage(models.Model):
+    SERPOrder = models.PositiveSmallIntegerField()
+    totalTimeOverSearchPage = models.FloatField(blank=True)  # in seconds
+    totalTimeOverSuggestionsBoard = models.FloatField(blank=True)  # in seconds
+    timestamp = models.DateTimeField(auto_now_add=True)
+    url = models.URLField()
+    search = models.ForeignKey('Search', related_name='searchPages')
+
+
+class SearchResult(models.Model):
+    rank = models.PositiveSmallIntegerField()
+    link = models.URLField()
+    title = models.CharField(max_length=100)
+    snippet = models.TextField(blank=True)
+    searchPage = models.ForeignKey('SearchPage', related_name='searchResults')
+
+
+class WebPage(models.Model):
+    pageLoadTimestamp = models.DateTimeField(auto_now_add=True)
+    timeOnPage = models.FloatField(blank=True)
+    numScrollEvents = models.PositiveSmallIntegerField(blank=True)
+    searchResult = models.ManyToManyField('SearchResult', related_name='webPages')
+
+
+#############################
+#           Events          #
+#############################
+
+
+class Event(models.Model):
+    eventTimestamp = models.DateTimeField(auto_now_add=True)
+    type = models.ForeignKey('EventType', related_name='events')
+    searchPage = models.ForeignKey('SearchPage', related_name='events', blank=True, null=True)
+    webPage = models.ForeignKey('WebPage', related_name='events', blank=True, null=True)
+
+
+class EventType(models.Model):
+    type = models.CharField(max_length=20)
+
+
+class Copy(models.Model):
+    id = models.OneToOneField('Event', primary_key=True)
+    copyText = models.TextField()
+
+
+class Find(models.Model):
+    id = models.OneToOneField('Event', primary_key=True)
+    findText = models.CharField(max_length=50)
+
+
+class SwitchSE(models.Model):
+    id = models.OneToOneField('Event', primary_key=True)
+    origin = models.ForeignKey('SearchEngine')
+    destination = models.ForeignKey('SearchEngine')
+
+
+class Click(models.Model):
+    linkText = models.URLField(blank=True)
+    searchResult = models.ForeignKey('SearchResult', blank=True, null=True)
+    seRelatedSearch = models.ForeignKey('SERelatedSearch', blank=True, null=True)
+    Suggestion = models.ForeignKey('Suggestion', blank=True, null=True)
